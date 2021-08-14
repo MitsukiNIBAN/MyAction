7e844c72a486393dc42c2b9f0046e26c4b7679a1
Mitsuki <psnhewzc@gmail.com>
2021-08-14 15:03:39 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
-func main()  {
+func main() {
 	appVer := os.Args[1]
-	uid:= os.Args[2]
+	uid := os.Args[2]
 	cookie := os.Args[3]
-	actID:="e202009291139501"
+	actID := "e202009291139501"
 	region := "cn_gf01"
+
+	userAgent := fmt.Sprintf("Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) miHoYoBBS/%v", appVer)
+	referer := fmt.Sprintf("https://webstatic.mihoyo.com/bbs/event/signin-ys/index.html?bbs_auth_required=%v&act_id=%v&utm_source=%v&utm_medium=%v&utm_campaign=%v",
+		"true", actID, "bbs", "mys", "icon")
 
 	infoUrl := fmt.Sprintf("https://api-takumi.mihoyo.com/event/bbs_sign_reward/info?region=%v&act_id=%v&uid=%v", region, actID, uid)
 	// signUrl := "https://api-takumi.mihoyo.com/event/bbs_sign_reward/sign"
 
-	fmt.Println(infoUrl)
-	fmt.Println(appVer)
-	fmt.Println(cookie)
+	infoReq, _ := http.NewRequest("GET", infoUrl, nil)
+	infoReq.Header.Set("User-Agent", userAgent)
+	infoReq.Header.Set("Referer", referer)
+	infoReq.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	infoReq.Header.Set("Cookie", cookie)
+	infoResp, err := (&http.Client{}).Do(infoReq)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer infoResp.Body.Close()
+	respByte, _ := ioutil.ReadAll(infoResp.Body)
+	fmt.Println(string(respByte))
 }
